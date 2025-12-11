9e80c9597b9db1c607c99035ee04cb62fa8cba18
Shreya-das2002 <das.shreya.sid@gmail.com>
2025-12-11 21:32:08 +0530

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold bg-amber-400 text-blue-600">React + TS + Vite + Tailwind v4</h1>
+    </div>
+  );
+}
+
+export default App;
