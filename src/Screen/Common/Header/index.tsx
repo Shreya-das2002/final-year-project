858b1524471931b4e09ee561f6cbac748c5ccd52
Hi-mang-shu <official.for.subhankar111@gmail.com>
2025-12-12 19:29:24 +0530

    dark

--- src/Screen/Common/Header/index.tsx
+++ src/Screen/Common/Header/index.tsx
@@ -39,8 +39,8 @@ const Header: React.FC = () => {
       {/* TOP HEADER */}
       <header className="w-full h-16 bg-gradient-to-r from-blue-100 via-blue-400 to-blue-700 dark:from-gray-400 dark:via-gray-500 dark:to-gray-700 flex items-center justify-between px-6 shadow-md">
         <div>
-          <span className="text-xl text-blue-950 font-bold">Sympto</span>
-          <span className="text-xl text-sky-600 font-bold">Nexus</span>
+          <span className="text-xl text-blue-950 dark:text-sky-600 font-bold">Sympto</span>
+          <span className="text-xl text-sky-600 dark:text-blue-950 font-bold">Nexus</span>
         </div>
 
 
@@ -73,21 +73,21 @@ const Header: React.FC = () => {
       {/* NAV BAR */}
       <nav className="w-full h-18 bg-blue-100 flex items-center justify-between px-6 shadow">
         <div className="flex items-center">
-          <span className="text-xl text-blue-950 font-bold">Guiding Your Path</span>
-          &ensp;
-          <span className="text-xl text-blue-500 font-bold">From Concern to Calm</span>
+          <span className="text-xl text-blue-500 dark:text-blue-950 font-bold">Guiding Your Path</span>
+          &ensp; 
+          <span className="text-xl text-blue-950 dark:text-sky-600 font-bold">From Concern to Calm</span>
         </div>
 
         <div className="flex gap-4">
-          <button className="px-4 py-1 h-12 rounded-lg bg-gradient-to-r from-blue-400 to-blue-700 text-white font-semibold">
+          <button className="px-4 py-1 h-12 rounded-lg bg-gradient-to-r from-blue-400 to-blue-700 dark:from-gray-500 dark:to-gray-700 text-white font-semibold">
             Doctor
           </button>
 
-          <button className="px-4 py-1 h-12 rounded-lg bg-gradient-to-r from-blue-400 to-blue-700 text-white font-semibold">
+          <button className="px-4 py-1 h-12 rounded-lg bg-gradient-to-r from-blue-400 to-blue-700 dark:from-gray-500 dark:to-gray-700 text-white font-semibold">
             Patient
           </button>
 
-          <button className="px-4 py-1 h-12 rounded-lg bg-gradient-to-r from-blue-400 to-blue-700 text-white font-semibold">
+          <button className="px-4 py-1 h-12 rounded-lg bg-gradient-to-r from-blue-400 to-blue-700 dark:from-gray-500 dark:to-gray-700 text-white font-semibold">
             Admin
           </button>
         </div>
