b3e102663ee0d732d7faeda178a64ba006961c50
Hi-mang-shu <official.for.subhankar111@gmail.com>
2025-12-15 21:37:49 +0530

    footer g900 - g800

--- src/Screen/Common/Footer/index.tsx
+++ src/Screen/Common/Footer/index.tsx
@@ -14,7 +14,7 @@ const Footer: React.FC = () => {
   };
 
   return (
-    <footer className="w-full bg-gray-200 dark:bg-gray-900 text-gray-600 dark:text-gray-400 py-4 text-center">
+    <footer className="w-full bg-gray-200 dark:bg-gray-800 text-gray-600 dark:text-gray-400 py-4 text-center">
       <div className="container mx-auto">
         <p className="mb-2">
           © {new Date().getFullYear()} SymptoNexus. All rights reserved.
