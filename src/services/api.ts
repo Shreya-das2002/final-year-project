2646e16268a307963f3b5bb552d31727e8a371e4
Shreya-das2002 <das.shreya.sid@gmail.com>
2026-01-07 15:23:19 +0530

    api update

---
+++ src/services/api.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const API = axios.create({
+  baseURL: "/api",
+  headers: { "Content-Type": "application/json" }
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  return config;
+});
