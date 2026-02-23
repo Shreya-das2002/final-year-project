2716f7b757f311d3c53c552d1ca77ff20ee103c3
Shreya-das2002 <das.shreya.sid@gmail.com>
2026-02-23 19:15:15 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -51,6 +51,7 @@ import DoctorViewProfile from "./Screen/Admin/DoctorProfile/DoctorViewProfile";
 import SpDoctorList from "./Screen/Patient/Appointments/DoctorList";
 import AdminProfileView from "./Screen/Admin/AdminProfile/AdminViewProfile";
 import AdminEditProfile from "./Screen/Admin/AdminProfile/AdminEditProfile";
+import SlotAvailability from "./Screen/Admin/SlotAvailability";
 
 /* ================= LAYOUT (SAFE PLACE FOR useLocation) ================= */
 
@@ -164,6 +165,7 @@ const AppLayout: React.FC = () => {
               <Route path="doctor_list" element={<DoctorList/>} />
               <Route path="messages" element={<Messages/>} />
               <Route path="add_doctor" element={<AddDoctor/>} />
+              <Route path="slot_availability"element= {<SlotAvailability/>} />
               <Route path="doctor_view_profile/:doctorId" element={<DoctorViewProfile />} />
               <Route path="doctor_edit_profile/:doctorId" element={<DoctorEditProfile/>} />
               <Route path="admin_view_profile/:id" element={<AdminProfileView />} />
