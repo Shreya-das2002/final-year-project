10d6830fda9843a6ef140c146483975191754083
Hi-mang-shu <official.for.subhankar111@gmail.com>
2026-03-13 16:56:30 +0530

    footer

--- src/Screen/Common/Footer/index.tsx
+++ src/Screen/Common/Footer/index.tsx
@@ -15,7 +15,7 @@ const Footer: React.FC = () => {
 
   return (
 
-    <footer className="fixed bottom-0 left-0 w-full h-12 py-3 px-6 bg-linear-to-r from-sky-100 to-blue-300 dark:from-gray-700 dark:to-gray-800 z-50">
+    <footer className="fixed bottom-0 left-0 w-full h-12 py-3 px-6 bg-linear-to-r from-sky-200 via-cyan-600 to-cyan-800 dark:from-slate-500 dark:via-sky-800 dark:to-sky-950 z-50">
 
       <div className="mx-w-7xl mx-auto px-6 flex items-center justify-between">
         <p className="text-black dark:text-gray-400 text-sm">
@@ -25,35 +25,35 @@ const Footer: React.FC = () => {
         <div className="flex items-center gap-6 text-sm">
           <span 
             onClick={() => handleNavigate("/about")}
-            className="text-black dark:text-gray-400 hover:text-green-800 dark:hover:text-teal-600 cursor-pointer"
+            className="text-white dark:text-gray-200 hover:text-green-400 dark:hover:text-teal-500 cursor-pointer"
           >
             About
           </span>
 
           <span 
             onClick={() => handleNavigate("/privacy")}
-            className="text-black dark:text-gray-400 hover:text-green-800 dark:hover:text-teal-600 cursor-pointer"
+            className="text-white dark:text-gray-200 hover:text-green-400 dark:hover:text-teal-500 cursor-pointer"
           >
             Privacy Policy
           </span>
 
           <span 
             onClick={() => handleNavigate("/contact")}
-            className="text-black dark:text-gray-400 hover:text-green-800 dark:hover:text-teal-600 cursor-pointer"
+            className="text-white dark:text-gray-200 hover:text-green-400 dark:hover:text-teal-500 cursor-pointer"
           >
             Contact
           </span>
 
           <span 
             onClick={() => handleNavigate("/faq")}
-            className="text-black dark:text-gray-400 hover:text-green-800 dark:hover:text-teal-600 cursor-pointer"
+            className="text-white dark:text-gray-200 hover:text-green-400 dark:hover:text-teal-500 cursor-pointer"
           >
             FAQs
           </span>
 
           <span 
             onClick={() => handleNavigate("/apply_doctor")}
-            className="text-black dark:text-gray-400 hover:text-green-800 dark:hover:text-teal-600 cursor-pointer"
+            className="text-white dark:text-gray-200 hover:text-green-400 dark:hover:text-teal-500 cursor-pointer"
           >
             Join as Doctor
           </span>
